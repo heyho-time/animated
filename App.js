a6e1b01328cd947a544145470d058dc538f4a2a8
성해호 <harry@seonghaehoui-iMac.local>
2022-01-20 16:57:35 +0900

    Feat : 스프링 애니메이션

--- App.js
+++ App.js
@@ -1,93 +1,65 @@
-import React, { useEffect, useState, useRef } from "react";
-import {
-  Animated,
-  Easing,
-  TouchableOpacity,
-  Pressable,
-  Dimensions,
-} from "react-native";
+import React, { useRef } from "react";
+import { Animated, PanResponder } from "react-native";
 import styled from "styled-components/native";
-
-const AnimatedBox = Animated.createAnimatedComponent(Box);
-
-const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
-
-export default function App() {
-  const POSITION = useRef(
-    new Animated.ValueXY({
-      x: -SCREEN_WIDTH / 2 + 100,
-      y: -SCREEN_HEIGHT / 2 + 100,
-    })
-  ).current;
-
-  const topLeft = Animated.timing(POSITION, {
-    toValue: {
-      x: -SCREEN_WIDTH / 2 + 100,
-      y: -SCREEN_HEIGHT / 2 + 100,
-    },
-    useNativeDriver: false,
-  });
-  const bottomLeft = Animated.timing(POSITION, {
-    toValue: {
-      x: -SCREEN_WIDTH / 2 + 100,
-      y: SCREEN_HEIGHT / 2 - 100,
-    },
-    useNativeDriver: false,
-  });
-  const bottomRight = Animated.timing(POSITION, {
-    toValue: {
-      x: SCREEN_WIDTH / 2 - 100,
-      y: SCREEN_HEIGHT / 2 - 100,
-    },
-    useNativeDriver: false,
-  });
-  const topRight = Animated.timing(POSITION, {
-    toValue: {
-      x: SCREEN_WIDTH / 2 - 100,
-      y: -SCREEN_HEIGHT / 2 + 100,
-    },
-    useNativeDriver: false,
-  });
-
-  const moveUp = () => {
-    Animated.loop(
-      Animated.sequence([bottomLeft, bottomRight, topRight, topLeft])
-    ).start();
-  };
-
-  const borderRadius = POSITION.y.interpolate({
-    inputRange: [-300, 300],
-    outputRange: [100, 0],
-  });
-
-  const bgColor = POSITION.y.interpolate({
-    inputRange: [-300, 300],
-    outputRange: ["rgb(255, 99, 71)", "rgb(71, 166, 255)"],
-  });
-
-  return (
-    <Container>
-      <Pressable onPress={moveUp}>
-        <AnimatedBox
-          style={{
-            backgroundColor: bgColor,
-            borderRadius,
-            transform: [...POSITION.getTranslateTransform()],
-          }}
-        />
-      </Pressable>
-    </Container>
-  );
-}
 
 const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
 `;
-
 const Box = styled.View`
-  background-color: red;
+  background-color: tomato;
   width: 200px;
   height: 200px;
 `;
+
+const AnimatedBox = Animated.createAnimatedComponent(Box);
+export default function App() {
+  const POSITION = useRef(
+    new Animated.ValueXY({
+      x: 0,
+      y: 0,
+    })
+  ).current;
+  const borderRadius = POSITION.y.interpolate({
+    inputRange: [-300, 300],
+    outputRange: [100, 0],
+  });
+  const bgColor = POSITION.y.interpolate({
+    inputRange: [-300, 300],
+    outputRange: ["rgb(255, 99, 71)", "rgb(71, 166, 255)"],
+  });
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (_, { dx, dy }) => {
+        POSITION.setValue({
+          x: dx,
+          y: dy,
+        });
+      },
+      onPanResponderRelease: () => {
+        Animated.spring(POSITION, {
+          toValue: {
+            x: 0,
+            y: 0,
+          },
+          bounciness: 20,
+          useNativeDriver: false,
+        }).start();
+      },
+    })
+  ).current;
+  return (
+    <Container>
+      <AnimatedBox
+        {...panResponder.panHandlers}
+        style={{
+          borderRadius,
+          backgroundColor: bgColor,
+          transform: POSITION.getTranslateTransform(),
+        }}
+      />
+    </Container>
+  );
+}
